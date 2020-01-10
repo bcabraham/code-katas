140433123b854de21da4852bc5bab4265a15ff02
bcabraham <abraham.bryan@gmail.com>
2020-01-09 18:04:51 -0700

    changed some file and directory names to match current convention

--- src/01-disemvowel/index.test.js
+++ src/01-disemvowel/index.test.js
@@ -1,5 +1,5 @@
 const assert = require("chai").assert;
-const { disemvowel } = require("./main");
+const {disemvowel} = require("./main");
 
 describe("disemvowel()", function() {
   it("should return a string with all vowels removed", function() {
